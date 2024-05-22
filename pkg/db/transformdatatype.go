e0257bea7c9456546db138cd6096a2915244283b
Tianchen Fu <tianchen.fu@dolphindb.com>
2024-05-22 15:10:00 +0800

    refactor: data type transform

--- pkg/db/transformdatatype.go
+++ pkg/db/transformdatatype.go
@@ -2,12 +2,42 @@ package db
 
 import (
 	"errors"
+	"reflect"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dolphindb/api-go/model"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 )
+
+// 类型映射，将 model.DataType 映射到对应的 Go 类型
+var typeMap = map[model.DataTypeByte]reflect.Type{
+	model.DtDate:   reflect.TypeOf(time.Time{}),
+	model.DtTime:   reflect.TypeOf(time.Time{}),
+	model.DtDouble: reflect.TypeOf(float64(0)),
+}
+
+// convertValue 将值转换为指定类型
+func convertValue(val interface{}, targetType reflect.Type) (reflect.Value, error) {
+	value := reflect.ValueOf(val)
+	if !value.Type().ConvertibleTo(targetType) {
+		return reflect.Value{}, errors.New("type assertion failed")
+	}
+	return value.Convert(targetType), nil
+}
+
+// convertSlice 转换切片中的元素类型
+func convertSlice(input []interface{}, targetType reflect.Type) (interface{}, error) {
+	output := reflect.MakeSlice(reflect.SliceOf(targetType), len(input), len(input))
+	for i, val := range input {
+		convertedValue, err := convertValue(val, targetType)
+		if err != nil {
+			return nil, err
+		}
+		output.Index(i).Set(convertedValue)
+	}
+	return output.Interface(), nil
+}
 
 // TransformVector 将 Vector 转换为一个包含具体类型元素的切片
 func TransformVector(vector *model.Vector) (interface{}, error) {
@@ -16,42 +46,15 @@ func TransformVector(vector *model.Vector) (interface{}, error) {
 	vectorValue := vector.GetRawValue()
 	vectorTypeString := vector.GetDataTypeString()
 
-	log.DefaultLogger.Info(spew.Sdump(vectorType))
-	log.DefaultLogger.Info(spew.Sdump(vectorTypeString))
+	// 用于调试数据类型转换
+	log.DefaultLogger.Debug(spew.Sdump(vectorType))
+	log.DefaultLogger.Debug(spew.Sdump(vectorTypeString))
 
-	// 根据类型进行转换
-	switch vectorType {
-	case model.DtDate:
-		arr := make([]time.Time, len(vectorValue))
-		for i, val := range vectorValue {
-			item, ok := val.(time.Time)
-			if !ok {
-				return nil, errors.New("type assertion to time.Time failed")
-			}
-			arr[i] = item
-		}
-		return arr, nil
-	case model.DtTime:
-		arr := make([]time.Time, len(vectorValue))
-		for i, val := range vectorValue {
-			item, ok := val.(time.Time)
-			if !ok {
-				return nil, errors.New("type assertion failed")
-			}
-			arr[i] = item
-		}
-		return arr, nil
-	case model.DtDouble:
-		arr := make([]float64, len(vectorValue))
-		for i, val := range vectorValue {
-			item, ok := val.(float64)
-			if !ok {
-				return nil, errors.New("type assertion failed")
-			}
-			arr[i] = item
-		}
-		return arr, nil
+	// 根据类型映射进行转换
+	targetType, ok := typeMap[vectorType]
+	if !ok {
+		return nil, errors.New("unsupported vector type")
 	}
 
-	return make([]int,len(vectorValue)), errors.New("unable to transform vector")
+	return convertSlice(vectorValue, targetType)
 }
